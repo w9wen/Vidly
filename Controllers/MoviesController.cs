37fc04f9754192be7ee5426ce79ea18c037abdee
William (PC) <william.wen@shin-her.com.tw>
2020-02-01 20:39:47 +0800

    新增Action並測試參數。

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -9,11 +9,27 @@ namespace Vidly.Controllers
         public IActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            // return View(movie);
+            return View(movie);
             // return Content("Hello World!");
             // return NotFound();
             // return new EmptyResult();
-            return  RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            // return  RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
