a1cf2aef365a0069724702e2ecac104ef87da270
William (PC) <william.wen@shin-her.com.tw>
2020-02-01 20:06:01 +0800

    Return Action練習

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -6,10 +6,14 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // Get: Movies/Random
-        public ActionResult Random()
+        public IActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            // return View(movie);
+            // return Content("Hello World!");
+            // return NotFound();
+            // return new EmptyResult();
+            return  RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
         }
     }
 }
