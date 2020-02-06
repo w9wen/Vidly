21c74011115e1687402c0483229b8ebe19ab263d
William (PC) <william.wen@shin-her.com.tw>
2020-02-07 05:18:41 +0800

    顯示由資料庫取得的資料

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,21 +1,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Data;
 using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext dbContext;
+
+        public CustomersController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.dbContext.Dispose();
+        }
+
         public IActionResult Index()
         {
-            var customers = GetCustomers();
+            // var customers = GetCustomers();
+            var customers = this.dbContext.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
 
         public IActionResult Detail(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = this.dbContext.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
