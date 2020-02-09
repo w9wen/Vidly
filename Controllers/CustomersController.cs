e48d6fde4fe598c51ae6a7e3e88f2287928b7910
William (PC) <william.wen@shin-her.com.tw>
2020-02-09 09:14:40 +0800

    新增資料頁面的練習

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,10 +1,9 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Vidly.Data;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -42,13 +41,46 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
+
+        public async Task<IActionResult> New()
         {
-            return new List<Customer>()
+            var membershipTypes = await dbContext.MembershipTypes.ToListAsync();
+            var newCustomer = new CustomerFormViewModel()
             {
-                new Customer(){ Id = 1, Name = "John Smith"},
-                new Customer(){ Id = 2, Name = "Mary William"},
+                MembershipTypes = membershipTypes
             };
+            return View("CustomerForm", newCustomer);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            await dbContext.Customers.AddAsync(customer);
+            await dbContext.SaveChangesAsync();
+            return View("Test");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var customer = await dbContext.Customers.SingleOrDefaultAsync(c => c.Id == id);
+            if (customer == null)
+                return NotFound();
+
+                var newCustomer = new CustomerFormViewModel()
+                {
+                    Customer = customer,
+                    MembershipTypes = await dbContext.MembershipTypes.ToListAsync()
+                };
+            return View("CustomerForm", newCustomer);
+        }
+
+        // private IEnumerable<Customer> GetCustomers()
+        // {
+        //     return new List<Customer>()
+        //     {
+        //         new Customer(){ Id = 1, Name = "John Smith"},
+        //         new Customer(){ Id = 2, Name = "Mary William"},
+        //     };
+        // }
     }
 }
