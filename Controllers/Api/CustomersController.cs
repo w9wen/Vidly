a9e2b804733f0def2a74278affd98a337c408c18
William (PC) <william.wen@shin-her.com.tw>
2020-02-12 06:06:53 +0800

    建立RESTful練習。

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -22,20 +22,105 @@ namespace Vidly.Controllers.Api
             this.dbContext = dbContext;
         }
 
-        // GET /api/customers
-        public async Task<IEnumerable<Customer>> GetCustomers()
+        // POST /api/customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
         {
             return await this.dbContext.Customers.ToListAsync();
         }
 
         // GET /api/customers/1
+        [HttpGet("{id}")]
         public async Task<ActionResult<Customer>> GetCustomer(int id)
         {
-            var customer = await this.dbContext.Customers.SingleOrDefaultAsync(c => c.Id == id);
+            // var customer = await this.dbContext.Customers.SingleOrDefaultAsync(c => c.Id == id);
+            var customer = await this.dbContext.Customers.FindAsync(id);
+
             if (customer == null)
                 return NotFound();
 
             return customer;
         }
+
+
+        // POST /api/customers
+        [HttpPost]
+        // [ProducesResponseType(StatusCodes.Status201Created)]
+        // [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Customer>> CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            this.dbContext.Customers.Add(customer);
+            await this.dbContext.SaveChangesAsync();
+
+            // return customer;
+            return CreatedAtAction(nameof(GetCustomer), new {id = customer.Id}, customer);
+        }
+
+        // PUT /api/customers/1
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCustomer(int id, Customer customer)
+        {
+            // if (!ModelState.IsValid)
+            //     return BadRequest();
+
+            // var customerInDb = await this.dbContext.Customers.SingleOrDefaultAsync(c => c.Id == id);
+            // if (customerInDb == null)
+            //     return NotFound();
+
+            // customerInDb.Name = customer.Name;
+            // customerInDb.Birthdate = customer.Birthdate;
+            // customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            // customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            // await this.dbContext.SaveChangesAsync();
+            // return Ok();
+            if (id != customer.Id)
+                return BadRequest();
+
+            this.dbContext.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await this.dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else throw;
+            }
+            return NoContent();
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Customer>> DeleteCustomer(int id)
+        {
+            // // var customerInDb = await this.dbContext.Customers.SingleOrDefaultAsync(c => c.Id == id);
+            
+            // if (customerInDb == null)
+            //     return NotFound();
+
+            // this.dbContext.Customers.Remove(customerInDb);
+            // await this.dbContext.SaveChangesAsync();
+            // return Ok();
+
+            var customer = await this.dbContext.Customers.FindAsync(id);
+            if(customer == null)
+             return NotFound();
+
+             this.dbContext.Customers.Remove(customer);
+             await this.dbContext.SaveChangesAsync();
+
+             return customer;
+
+        }
+
+        private bool CustomerExists(int id) => this.dbContext.Customers.Any(c => c.Id == id);
     }
 }
